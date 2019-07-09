2c52503fb7456ee801c4331842cd9c3f9597afae
Shuvra Das <shuvradas59@gmail.com>
2019-07-10 00:23:06 +0600

    Add files via upload

---
+++ 4.Print Hello World!.asm
@@ -0,0 +1,28 @@
+.MODEL SMALL
+.STACK 100H
+.DATA 
+MSG1 DB "HELLO WORLD!$"
+.CODE
+
+MAIN PROC  
+    
+    
+    
+    
+     MOV AX,@DATA
+     MOV DS,AX             ;ITS DATA SAGEMENT(DS)
+     
+     LEA DX,MSG1
+     MOV AH,9              ;USE AH,9 FOR STRING OUTPUT
+     INT 21H 
+     
+     
+     
+     
+     
+    
+     EXIT:
+     MOV AH,4CH
+     INT 21H
+     MAIN ENDP
+     END MAIN
